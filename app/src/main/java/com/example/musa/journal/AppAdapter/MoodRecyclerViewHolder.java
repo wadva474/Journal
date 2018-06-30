51b5313a1d12a40f9843e98690051067a5545306
wadva474 <wadva474@gmail.com>
2018-06-30 03:40:05 +0100

    Using Firebase Database and FireBase RecyclerView

---
+++ app/src/main/java/com/example/musa/journal/AppAdapter/MoodRecyclerViewHolder.java
@@ -0,0 +1,29 @@
+package com.example.musa.journal.AppAdapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.musa.journal.R;
+
+public class MoodRecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public TextView mMood;
+    public   TextView mDescription;
+    public ImageView mImage;
+
+
+    public MoodRecyclerViewHolder(View itemView) {
+        super(itemView);
+        mMood= itemView.findViewById(R.id.moodView);
+        mDescription= itemView.findViewById(R.id.descriptionView);
+        mImage=itemView.findViewById(R.id.showimageView);
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int position=getAdapterPosition();
+    }
+}
---
+++ app/src/main/java/com/example/musa/journal/ImageUriUtils/BitmapUtils.java
@@ -0,0 +1,24 @@
+package com.example.musa.journal.ImageUriUtils;
+
+import android.content.Context;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class BitmapUtils {
+    public static File createTempImageFile(Context context) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalCacheDir();
+
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+    }
+}
